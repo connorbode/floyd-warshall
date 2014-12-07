5edbb22ca94e042475291f275cb02b500aee4cc1
Connor Bode <connor.bode@gmail.com>
2014-12-07 17:02:35 -0500

    working pipelining

--- parallel-pipeline/parallel-pipeline.c
+++ parallel-pipeline/parallel-pipeline.c
@@ -47,6 +47,8 @@ int main (int argc, const char *argv[]) {
   MPI_Status col_status;
   int row_flag;
   int col_flag;
+  int curr_cost;
+  int alt_cost;
 
   //-- GATHERING THE FINAL MATRIX
   int *out_of_order_subblocks;
@@ -155,7 +157,9 @@ int main (int argc, const char *argv[]) {
     kth_rank = k / subblock_dimensions;
     kth_row = (int*) malloc (sizeof(int) * subblock_dimensions);
     kth_col = (int*) malloc (sizeof(int) * subblock_dimensions);
+    next_subblock = (int*) malloc (sizeof(int) * subblock_dimensions * subblock_dimensions);
 
+    // compile & send or initialize receive kth_row
     if (grid_rank_i == kth_rank) {
       received_kth_row = 1;
       for (i = 0; i < subblock_dimensions; i += 1) {
@@ -173,6 +177,7 @@ int main (int argc, const char *argv[]) {
       row_flag = 0;
     }
 
+    // compile & send or initialize receive of the kth_col
     if (grid_rank_j == kth_rank) {
       received_kth_col = 1;
       for (i = 0; i < subblock_dimensions; i += 1) {
@@ -182,15 +187,15 @@ int main (int argc, const char *argv[]) {
         MPI_Send(kth_col, subblock_dimensions, MPI_INT, grid_rank_j + 1, k, comm_row);
       }
       if (grid_rank_j > 0) {
-        MPI_Send(kth_row, subblock_dimensions, MPI_INT, grid_rank_j - 1, k, comm_row);
+        MPI_Send(kth_col, subblock_dimensions, MPI_INT, grid_rank_j - 1, k, comm_row);
       }
     } else {
       MPI_Irecv(kth_col, subblock_dimensions, MPI_INT, MPI_ANY_SOURCE, k, comm_row, &col_request);
       col_flag = 0;
     }
 
+    // wait on receiving the kth row / col if we don't already have them.
     while (! (received_kth_row && received_kth_col)) {
-
       if (!received_kth_row) {
         MPI_Test(&row_request, &row_flag, &row_status);
         if (row_flag) {
@@ -204,71 +209,83 @@ int main (int argc, const char *argv[]) {
           received_kth_col = 1;
         }
       }
-
     }
 
+
+    // Build next subblock
+    for (i = 0; i < subblock_dimensions; i += 1) {
+      for (j = 0; j < subblock_dimensions; j += 1) {
+        curr_cost = subblock[i * subblock_dimensions + j];
+        alt_cost = kth_row[j] + kth_col[i];
+        next_subblock[i * subblock_dimensions + j] = curr_cost < alt_cost ? curr_cost : alt_cost;
+      }
+    }
+
+    free(subblock);
     free(kth_row);
     free(kth_col);
+
+    subblock = next_subblock;
   }
 
   // GATHER
-  // if (IS_MASTER) {
-  //   out_of_order_subblocks = (int*) malloc(sizeof(int) * size * subblock_dimensions * subblock_dimensions);
-  //   gather_counts = (int*) malloc(sizeof(int) * size);
-  //   gather_offsets = (int*) malloc(sizeof(int) * size);
-  //   for (i = 0; i < size; i += 1) {
-  //     gather_counts[i] = subblock_dimensions * subblock_dimensions;
-  //     gather_offsets[i] = gather_counts[i] * i;
-  //   }
-  // }
-  // MPI_Gatherv(subblock, subblock_dimensions * subblock_dimensions, MPI_INT, out_of_order_subblocks, gather_counts, gather_offsets, MPI_INT, MASTER, MPI_COMM_WORLD);
+  if (IS_MASTER) {
+    out_of_order_subblocks = (int*) malloc(sizeof(int) * size * subblock_dimensions * subblock_dimensions);
+    gather_counts = (int*) malloc(sizeof(int) * size);
+    gather_offsets = (int*) malloc(sizeof(int) * size);
+    for (i = 0; i < size; i += 1) {
+      gather_counts[i] = subblock_dimensions * subblock_dimensions;
+      gather_offsets[i] = gather_counts[i] * i;
+    }
+  }
+  MPI_Gatherv(subblock, subblock_dimensions * subblock_dimensions, MPI_INT, out_of_order_subblocks, gather_counts, gather_offsets, MPI_INT, MASTER, MPI_COMM_WORLD);
 
-  // // Reorder the subblocks
-  // if (IS_MASTER) {
-  //   i = 0;
-  //   j = 0;
-  //   k = 0;
-  //   bump = 0;
-  //   for (written = 0; written < matrix_dimensions * matrix_dimensions; written += 1) {
-  //     matrix[written] = out_of_order_subblocks[k * matrix_dimensions * subblock_dimensions + j * subblock_dimensions * subblock_dimensions + i + bump];
-  //     i += 1;
-  //     if (i % subblock_dimensions == 0) {
-  //       i = 0;
-  //       j += 1;
-  //       if (j % grid_dimensions == 0) {
-  //         j = 0;
-  //         bump += subblock_dimensions;
-  //         if (bump == subblock_dimensions * subblock_dimensions) {
-  //           bump = 0;
-  //           k += 1;
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
+  // Reorder the subblocks
+  if (IS_MASTER) {
+    i = 0;
+    j = 0;
+    k = 0;
+    bump = 0;
+    for (written = 0; written < matrix_dimensions * matrix_dimensions; written += 1) {
+      matrix[written] = out_of_order_subblocks[k * matrix_dimensions * subblock_dimensions + j * subblock_dimensions * subblock_dimensions + i + bump];
+      i += 1;
+      if (i % subblock_dimensions == 0) {
+        i = 0;
+        j += 1;
+        if (j % grid_dimensions == 0) {
+          j = 0;
+          bump += subblock_dimensions;
+          if (bump == subblock_dimensions * subblock_dimensions) {
+            bump = 0;
+            k += 1;
+          }
+        }
+      }
+    }
+  }
 
-  // // print output 
-  // if (IS_MASTER) {
-  //   printf("OUTPUT MATRIX\n\n");
-  //   for (i = 0; i < matrix_dimensions; i += 1) {
-  //     for (j = 0; j < matrix_dimensions; j += 1) {
-  //       printf("%d ", matrix[i * matrix_dimensions + j]);
-  //     }
-  //     printf("\n");
-  //   }
-  //   printf("\n");
-  // }
+  // print output 
+  if (IS_MASTER) {
+    printf("OUTPUT MATRIX\n\n");
+    for (i = 0; i < matrix_dimensions; i += 1) {
+      for (j = 0; j < matrix_dimensions; j += 1) {
+        printf("%d ", matrix[i * matrix_dimensions + j]);
+      }
+      printf("\n");
+    }
+    printf("\n");
+  }
 
-  // // print output to file
-  // if (IS_MASTER) {
-  //   file = fopen(argv[2], "w");
-  //   for (i = 0; i < matrix_dimensions; i += 1) {
-  //     for (j = 0; j < matrix_dimensions; j += 1) {
-  //       fprintf(file, "%d ", matrix[i * matrix_dimensions + j]);
-  //     }
-  //     fprintf(file, "\n");
-  //   }
-  // }
+  // print output to file
+  if (IS_MASTER) {
+    file = fopen(argv[2], "w");
+    for (i = 0; i < matrix_dimensions; i += 1) {
+      for (j = 0; j < matrix_dimensions; j += 1) {
+        fprintf(file, "%d ", matrix[i * matrix_dimensions + j]);
+      }
+      fprintf(file, "\n");
+    }
+  }
 
   // finalize 
   free(matrix);
